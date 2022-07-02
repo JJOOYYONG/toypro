d5f99e7065ba05c509527b588345d8136189cddc
hyemi <hemi33379@gmail.com>
2022-07-02 20:55:01 +0900

    업데이트g

--- static/hemi.js
+++ static/hemi.js
@@ -1,1 +1,15 @@
 /*혜미님 만 쓰는 js*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  /*여기서부터*/
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  /*여기까지는 삭제 해도 됨.*/
+  <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
