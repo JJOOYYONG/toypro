3023fd2d3962a145d37b179fd7060d88b1f9552e
devyouth94 <devyouth94@gmail.com>
2022-07-08 11:19:59 +0900

    2022. 07. 08 1.리뷰리스트 저장 호출 구현 2.팝업창 css 수정 3.팝업창 html 수정 4.ajax 통신 구현, 팝업창 영화 제목 구현

--- static/youngjin.js
+++ static/youngjin.js
@@ -2,9 +2,14 @@
 
 $(document).ready(function() {
 	popup_close()
+	review_get()
 });
 
-function popup_on() {
+function popup_on(obj) {
+	$('#popup_title').empty()
+	let movie_title = $(obj).parent().children().first().text()
+	$('#popup_title').append(movie_title)
+
 	$("#popup_box").show();
 	$("#popup_mask").fadeIn(100);
 }
@@ -15,14 +20,51 @@ function popup_close() {
 }
 
 function popup_posting() {
+	let name = $('#popup_title').text()
+	let date = $('#date_select').val()
+	let star = $('#popup_star').val()
+	let comment = $('#popup_comment').val()
+
 	$.ajax({
 		type: "POST",
-		url: "/test",
-		data: {sample_give: '데이터전송'},
+		url: "/reviews",
+		data: {name_give: name, date_give: date, star_give: star, comment_give: comment},
 		success: function (response) {
 			alert(response['msg'])
 			popup_close()
 			window.location.reload()
 		}
 	})
+}
+
+function review_get() {
+	$.ajax({
+    type: "GET",
+    url: "/reviews",
+    data: {},
+    success: function(response){
+   		let popup_rows = response['reviews']
+		for(let i = 0; i < popup_rows.length; i++) {
+			let name = popup_rows[i]['name']
+			let date = popup_rows[i]['date']
+			let star = popup_rows[i]['star']
+			let comment = popup_rows[i]['comment']
+
+			let star_image = '⭐'.repeat(star)
+
+			let temp_html = `<div class="card mb-3">
+								<div class="row g-0">
+									<div class="col-md-12">
+										<div class="card-body">
+											<h5 class="card-title">${name} <span class="card_date">${date}</span></h5>
+											<p class="card-star">${star_image}</p>
+											<p class="card-text">${comment}</p>
+										</div>
+									</div>
+								</div>
+							</div>`
+			$('#review_div').append(temp_html)
+		}
+    }
+  })
 }
